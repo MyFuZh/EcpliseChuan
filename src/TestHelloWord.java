455df219c0bd9b7ffc9565bc92f811d4ef78b27c
yss-fuzheng <15624039605@163.com>
2017-09-15 10:09:26 +0800

    12 1

---
+++ src/TestHelloWord.java
@@ -0,0 +1,13 @@
+
+public class TestHelloWord {
+
+	public TestHelloWord() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
