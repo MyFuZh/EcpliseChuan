dacfa3d9d8eb41cb2cb26c54112af52a212ab4a5
yss-fuzheng <15624039605@163.com>
2017-09-15 10:15:18 +0800

    测试

--- src/TestHelloWord.java
+++ src/TestHelloWord.java
@@ -6,7 +6,8 @@ public class TestHelloWord {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("≤‚ ‘≤‚ ‘≤‚ ‘ ‘ ‘≤‚ ‘≤‚ ‘ ‘ ‘≤‚ ‘≤‚ ‘ ‘ ‘≤‚ ‘≤‚ ‘ ‘ ‘");
+		
 
 	}
 
